8eb70a1e281381f628b08d7f9974343441632991
Yingzhao Ouyang <KunojiLym@gmail.com>
2020-04-16 16:01:11 +0800

    Completed graph outputs for week ending 2 Feb 2019

--- project_01.py
+++ project_01.py
@@ -1,10 +1,26 @@
 """
 
-EDA of MTA data to determine best spots for 
+Helper functions for Metis project 1
 
 """
 
 import pandas as pd
 
-mta_test = pd.read_csv("http://web.mta.info/developers/data/nyct/turnstile/turnstile_190330.txt")
-mta_test.head()
+def find_weekend_indices(datetime_array):
+    """
+    Find the indices for weekends in datetime_array
+    """
+    indices = []
+    for i in range(len(datetime_array)):
+        if datetime_array[i].weekday() >= 5:
+            indices.append(i)
+    return indices
+
+def highlight_datetimes(indices, ax, data):
+    """
+    Highlight datetime backgrounds for data on ax based on selected indices 
+    """
+    i = 0
+    while i < len(indices)-1:
+        ax.axvspan(data.at[data.index[indices[i]], 'DATE'], data.at[data.index[indices[i] + 1], 'DATE'], facecolor='lightblue', edgecolor='none', alpha=.5)
+        i += 1
